edd7bf78591ebb049b61d3cb1fe61b60dec14432
zachee <zacheeishimwe@gmail.com>
2021-03-29 23:52:18 +0200

    short term momory of todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import React, {useState} from 'react';
+import './App.css';
 
 function App() {
+  // set a short term memory.
+  const [todos, setTodos] = useState(['Take dogs for a walk','Take Rubish out','back to bed']);
+  console.log(todos)
   return (
     <div className="App">
-    <h1>Hello word</h1>
+    <h1>Hello Clever Programmers</h1>
+    <input/>
+    <button>Add to do </button>
+
+    <ul>
+      {todos.map(todo => (
+        <li>{todo}</li>
+      ))}
+    </ul>
     </div>
-  );
+  ); 
 }
 
 export default App;

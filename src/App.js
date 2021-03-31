bdd21f3ac921a9a8f48be69d5980785bdc11fd42
zachee <zacheeishimwe@gmail.com>
2021-03-31 12:44:56 +0200

    add input todo in temporal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  // set a short term memory.
-  const [todos, setTodos] = useState(['Take dogs for a walk','Take Rubish out','back to bed']);
+  //short term memory.
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take Rubish out', 'back to bed']);
+  const [input, setInput] = useState('');
+  const addTodo = (event)=> {
+    // this will fire of when we add do!
+    setTodos([...todos,input])
+  }
   console.log(todos)
   return (
     <div className="App">
-    <h1>Hello Clever Programmers</h1>
-    <input/>
-    <button>Add to do </button>
-
-    <ul>
-      {todos.map(todo => (
-        <li>{todo}</li>
-      ))}
-    </ul>
+      <h1>Hello Clever Programmers</h1>
+      <input value={input} onChange={event=> setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add to do </button>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
-  ); 
+  );
 }
 
 export default App;

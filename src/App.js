5e0eca09447883ab9eb4f6555a7681d83b76ec5f
zachee <zacheeishimwe@gmail.com>
2021-03-31 14:22:03 +0200

    configure MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button'
 import './App.css';
 
 function App() {
   //short term memory.
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take Rubish out', 'back to bed']);
   const [input, setInput] = useState('');
-  const addTodo = (event)=> {
+  const addTodo = (event) => {
     // this will fire of when we add do!
     event.preventDefault(); // will stop the refresh 
-    setTodos([...todos,input])
+    setTodos([...todos, input])
+    setInput('');
   }
   console.log(todos)
   return (
     <div className="App">
       <h1>Hello Clever Programmers</h1>
       <form>
-      <input value={input} onChange={event=> setInput(event.target.value)}/>
-      <button type="submit" onClick={addTodo}>Add to do </button>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <Button type="submit" onClick={addTodo} variant="contained" color="primary">
+        Add to do
+        </Button>
+
+        {/* <button type="submit" onClick={addTodo}>Add to do </button> */}
       </form>
-
-
 
       <ul>
         {todos.map(todo => (
